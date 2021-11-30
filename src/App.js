552e451ba75d58cfe4082ce432b60ed0dfeb3486
satyamgupta1495 <satyamgupta1495@gmail.com>
2021-12-01 04:00:15 +0530

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import questions from "./components/Questions";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setshowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [questionNo, setQuestionNo] = useState(1);
+
+  const intervalRef = useRef();
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    if (count === 0) {
+      reset();
+    }
+    intervalRef.current = setTimeout(
+      () => setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0)),
+      1000
+    );
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [count]);
+
+  function reset() {
+    clearInterval(intervalRef.current);
+    nextQuestion();
+    setCount(5);
+  }
+
+  const nextQuestion = () => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setshowScore(true);
+    }
+    setQuestionNo(questionNo + 1);
+  };
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    nextQuestion();
+    setQuestionNo(questionNo + 1);
+    let trustScore = score + isCorrect;
+    setScore(trustScore);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {}
+      {showScore ? (
+        <div className="score-section">
+          Your Trust Score is: {score} / {questions.length * 10}{" "}
+        </div>
+      ) : (
+        <>
+          <div className="timer">{count}</div>
+          <div className="question-wrapper">
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {questionNo}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOptions) => (
+                <button
+                  onClick={() => {
+                    handleAnswerButtonClick(answerOptions.isCorrect);
+                    reset();
+                  }}
+                >
+                  {answerOptions.answerText}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div>{/* <button onClick={onClickResetButton}>Reset</button> */}</div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
